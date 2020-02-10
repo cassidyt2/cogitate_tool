c9d9dcd11d762fbd526a30ae5a7d80f70d1e53da
Claire Klima <klimac@allegheny.edu>
2020-02-10 16:25:24 -0500

    Added features that list issues and branches of GitHub repo.

--- demo.py
+++ demo.py
@@ -10,7 +10,16 @@ g = Github("access_token")
 g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
 
 for repo in g.get_user().get_repos():
-    print(repo.name)
+    print("Name of repository:\n" + repo.name)
     repo.edit(has_wiki=False)
     # to see all the available attributes and methods
     print(dir(repo))
+
+# return list of open issues
+repo = g.get_repo("PyGithub/PyGithub")
+open_issues = repo.get_issues(state="open")
+for issue in open_issues:
+    print(issue)
+
+# return list of branches
+list(repo.get_branches())
